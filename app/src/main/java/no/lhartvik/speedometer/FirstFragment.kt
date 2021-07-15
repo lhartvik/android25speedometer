8a9fced95eab32a3d3d521ba5fea7a017b89fe35
Lars Hartvigsen <lhartvik@gmail.com>
2021-07-16 01:36:22 +0200

    Sjekker posisjon en gang

--- app/src/main/java/no/lhartvik/speedometer/FirstFragment.kt
+++ app/src/main/java/no/lhartvik/speedometer/FirstFragment.kt
@@ -1,12 +1,20 @@
 package no.lhartvik.speedometer
 
+import android.annotation.SuppressLint
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import no.lhartvik.speedometer.databinding.FragmentFirstBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -18,14 +26,32 @@ class FirstFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+    private var prevLocation: Location? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("MissingPermission")
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-
+        requireContext()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        hvorErJeg()
+
         return binding.root
+    }
+
+    @SuppressLint("MissingPermission", "NewApi")
+    fun hvorErJeg() {
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                binding.textviewFirst.text = location.toString()
+            }.addOnCompleteListener {
+                binding.textviewFirst2.text = SimpleDateFormat("HH:MM:SS").format(Date())
+            }
 
     }
 
